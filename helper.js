c35591dca37810d19189505c4a0bfc4a2316f9fa
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2016-05-25 11:33:41 -0700

    Create helper.js

---
+++ helper.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+	//$('div').fadeOut('slow');
+	$('#button').click(function(){
+		var toAdd = $('input[name=checkListItem]').val();
+
+		$('.list').append("<li class= 'item'>"+toAdd+"</li>");
+	});
+
+
+	//this removes the whole list. how to just get it to remove 1 item?
+	// $(document).on('click', '.list', function() {
+	// 	$(this).remove();
+	// });
+
+
+
+	//this removes just the item you clicked. i looked up the on function to learn
+	//i needed to change my selector to 'li'
+	$(document).on('click', 'li', function() {
+		var lastItemAdded = $('li[-1]').val();
+		$(this).remove(lastItemAdded);
+	});
+});
